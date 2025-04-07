d5023e9ec53ccf3d8d70d47cae73bf6043731498
artiartem <a_shitikov8@mail.ru>
2025-04-08 02:54:50 +0700

    Added follow age of specified user feature

--- components/follow_age.py
+++ components/follow_age.py
@@ -1,8 +1,11 @@
-import twitchio
-import logging
 import datetime
+import logging
+
+from twitchio import ChannelFollowerEvent
 from twitchio.ext import commands
+
 from utils import format_time_russian
+
 
 class FollowAge(commands.Component):
     def __init__(self, bot):
@@ -10,14 +13,36 @@ class FollowAge(commands.Component):
         self.logger = logging.getLogger("FollowAge")
 
     @commands.command(name="followage")
-    async def follow_age(self, ctx: commands.Context) -> None:
-        follow_info: twitchio.ChannelFollowerEvent  = await ctx.chatter.follow_info()
+    async def follow_age(self, ctx: commands.Context, *, username: str = "") -> None:
+        username = username.strip().strip("󠀀").lstrip("@").lower()
+        if not username:
+            follow_info: ChannelFollowerEvent = await ctx.chatter.follow_info()
+            if follow_info is None:
+                return await ctx.reply(f"Вы не зафоловились на {ctx.broadcaster}!")
+            return await self._reply_with_follow_age(
+                ctx, follow_info, "Вы следите за этим каналом уже {formatted_age}!"
+            )
+
+        followers = await ctx.broadcaster.fetch_followers()
+        username = username.strip()
+        async for follow_info in followers.followers:
+            if follow_info.user.name.lower().strip() == username:
+                break
+        else:
+            return await ctx.reply("Такого фолловера не существует!")
+
+        await self._reply_with_follow_age(
+            ctx,
+            follow_info,
+            f"Пользователь {follow_info.user.mention} "
+            "следит за этим каналом уже {formatted_age}!",
+        )
+
+    async def _reply_with_follow_age(
+        self, ctx: commands.Context, follow_info: ChannelFollowerEvent, message: str
+    ):
         self.logger.debug(f"Follow info: {follow_info}")
-        
-        if follow_info is None:
-            return await ctx.reply(f"Ты не зафоловился на {ctx.broadcaster}!")
-            
         now = datetime.datetime.now(datetime.timezone.utc)
-        follow_age = (now - follow_info.followed_at)
-        
-        await ctx.reply(f"Вы следите за этим каналом уже {format_time_russian(follow_age.total_seconds())}!")
+        follow_age = now - follow_info.followed_at
+        formatted_age = format_time_russian(follow_age.total_seconds())
+        await ctx.reply(message.format(formatted_age=formatted_age))
