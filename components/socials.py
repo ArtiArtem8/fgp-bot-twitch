5a40f6a7e6f60f1877b15b2e043ec364990d04ed
artiartem <a_shitikov8@mail.ru>
2025-04-09 02:22:43 +0700

    Refactor ban command logic and add logging
    - Add logging better debugging.
    - Refactor the user lookup in the ban command by replacing the async loop with a list comprehension and next() for clarity.
    - Log the list of chatters when the target user is not found.
    - Update the ban notification message to inform that the action is irreversible (trolling).

--- components/socials.py
+++ components/socials.py
@@ -1,3 +1,5 @@
+import logging
+
 from twitchio.ext import commands
 
 from config import BOT_ID
@@ -6,6 +8,7 @@ from config import BOT_ID
 class Socials(commands.Component):
     def __init__(self, bot):
         self.bot = bot
+        self.logger = logging.getLogger("Socials")
 
     @commands.command(aliases=["ds", "дс", "дискорд"])
     async def discord(self, ctx: commands.Context) -> None:
@@ -17,18 +20,24 @@ class Socials(commands.Component):
 
     @commands.command(aliases=["бан", "удалить", "забанить"])
     async def ban(self, ctx: commands.Context, *, username: str = "") -> None:
-        username = username.strip().lstrip("@").lower()
+        username = username.strip().strip(" 󠀀").lstrip("@").lower()
 
         if username == "":
             username = ctx.author.display_name.lower()
 
         res = await ctx.broadcaster.fetch_chatters(moderator=BOT_ID)
-        async for user in res.users:
-            if str(user.display_name).lower() == username:
-                break
-        else:
+        chatters = [user async for user in res.users]
+        user = next(
+            (user for user in chatters if str(user.display_name).lower() == username),
+            None,
+        )
+
+        if user is None:
+            self.logger.info(
+                f"User not found, look for: {[user.display_name for user in chatters]}"
+            )
             return await ctx.reply("Такого чаттерса тут нет! Проверьте написание имени")
 
         await ctx.reply(
-            f"Внимание! Пользователь {user.mention} был удалён из чата! Обратитесь в администрацию для подачи апелляции!"
+            f"Внимание! Пользователь {user.mention} был удалён из чата! Это действие отменить нельзя!"
         )
